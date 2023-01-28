37d55316e621558f9fb10d8e05cdd0d1da116088
Lucas Tadeu Mendonça Ferreira <43001991+lucastmendoncaf@users.noreply.github.com>
2023-01-28 20:58:38 -0300

    Create OnlyLibrary.sh

---
+++ OnlyLibrary.sh
@@ -0,0 +1,18 @@
+###################################################################
+#Script Name	:  OnlyLibrarys                                                                                              
+#Description	:  instala as bibliotecas e faz as configurações necessarias para o sistema embarcado funcionar devidamente                                                                               
+#Args           	:                                                                                           
+#Author       	:Lucas Tadeu                                                
+#Email         	:lucastmendoncaf@gmail.com                                           
+###################################################################
+
+#sudo apt-get update
+#sudo apt-get upgrade
+#sudo apt-get install python3-pip
+#sudo apt-get install build-essential python3-dev python3-smbus python3-pip
+#sudo pip3 install --upgrade setuptools
+#sudo pip3 install --upgrade adafruit-python-shell
+#sudo pip3 install adafruit-blinka
+#sudo pip3 install adafruit-circuitpython-bno055
+#sudo pip3 install adafruit-circuitpython-vl53l0x
+#sudo pip3 install adafruit-ads1x15
